16ce91037a94a81a9874b0a534368c66eb03dbc2
Marlon <marlonvpaula@gmail.com>
2015-06-17 23:39:59 -0300

    Add new layout to pages sign in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require sb-admin-2
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
