cee0a2f652bf05f964f022ede3c230b4463cdaed
Marlon <marlonvpaula@gmail.com>
2015-10-03 16:55:40 -0300

    atualizado

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,12 @@
 require "refile/s3"
 
-aws = {
-  access_key_id: "111573216641817",
-  secret_access_key: "_Hn5cV0DVuhelxszbRd9bscRmeI",
+auth = {
+  api_key: "111573216641817",
+  api_secret: "_Hn5cV0DVuhelxszbRd9bscRmeI",
   # region: "sa-east-1",
-  bucket: "dje1zntec",
+  cloud_name: "dje1zntec",
 }
+
+#Cloudinary::Uploader.upload("my_picture.jpg", auth)
 #Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 #Refile.store = Refile::S3.new(prefix: "store", **aws)
