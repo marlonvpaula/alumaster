63c4d6897f3046885f0e25395f9f879da8b83148
Marlon <marlonvpaula@gmail.com>
2015-10-25 14:01:48 -0200

    configurando email alumaster

--- app/mailers/contato_mailer.rb
+++ app/mailers/contato_mailer.rb
@@ -5,9 +5,9 @@ class ContatoMailer < ActionMailer::Base
 		#@confirmation_link = root_url
 		@contact = contato
 		mail ({
-			to: 'marlonvpaula@gmail.com',
+			to: 'alumaster.toledo@gmail.com',
 			from: @contact.email,
-			#bcc: ['contatos <marlonvpaula@gmail.com>'], 
+			bcc: ['contatos <marlonvpaula@gmail.com>'], 
 			subject: 'Contato Alumaster'
 			})
 	end
