6494107d3a173379aa1230249bd9030bbad5cb09
Marlon <marlonvpaula@gmail.com>
2015-10-25 10:36:25 -0200

    configurando email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,9 @@ Alumaster::Application.configure do
     #  :port => ENV['MAILTRAP_PORT'],
     #  :authentication => :plain
     #}
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => '478874d96b2437828',
